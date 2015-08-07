3cfa49cbab9d647f9ef09074112cdb6398c6a55a
juliaPayschik <julia.payuschik@pri-num.com>
2015-08-07 17:49:30 +0300

    without spring security

--- backend/src/main/java/game/tictactoe/Application.java
+++ backend/src/main/java/game/tictactoe/Application.java
@@ -1,6 +1,5 @@
 package game.tictactoe;
 
-import game.tictactoe.security.AppSecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
